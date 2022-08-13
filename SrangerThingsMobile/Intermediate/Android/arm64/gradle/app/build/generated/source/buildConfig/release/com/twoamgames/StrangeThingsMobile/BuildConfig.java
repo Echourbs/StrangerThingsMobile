a4b83549b2589739cadac0eb5b895db7e64c8bef
Echourbs <felipe-reis14@hotmail.com>
2022-08-13 17:09:43 -0300

    FirstCommit

---
+++ SrangerThingsMobile/Intermediate/Android/arm64/gradle/app/build/generated/source/buildConfig/release/com/twoamgames/StrangeThingsMobile/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.twoamgames.StrangeThingsMobile;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.twoamgames.StrangeThingsMobile";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 34;
+  public static final String VERSION_NAME = "2.0";
+}
